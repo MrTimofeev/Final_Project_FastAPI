8294e6033fc8f0a981a0f214d74d2aade090b82a
MrTimofeev <nemo.capitan.26ru@mail.ru>
2025-08-21 16:14:28 +0300

    правки в main файле

--- app/main.py
+++ app/main.py
@@ -1,20 +1,17 @@
-from app.admin import setup_admin
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
-from app.database.database import Base, engine
-from app.api import auth, users, teams, tasks, meetings, evaluations, calendar
-from app.models.user import User
+from app.database.database import engine, Base
+from app.api import auth, users, teams, tasks, meetings, evaluations, calendar, frontend
 
 
 # Жизненный цикл приложения
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    # при старте создаем таблицы (если их нет)
     async with engine.begin() as conn:
         pass
     yield
@@ -42,26 +39,26 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 
-# Роут для проверки@
-@app.get("/")
-def read_root():
-    return {
-        "message": "Добро пожаловать в систему",
-        "docs": "/docs",
-        "admin": "/admin",
-        "login": "/auth/jwt/login"
-    }
-
-
+# # Роут для проверки@
+# @app.get("/")
+# def read_root():
+#     return {
+#         "message": "Добро пожаловать в систему",
+#         "docs": "/docs",
+#         "admin": "/admin",
+#         "login": "/auth/jwt/login"
+#     }
+    
+    
 # Подключаем роуты (авторизация, пользователи и т.д.)
-app.include_router(auth.router, prefix="", tags=["auth"])
-app.include_router(users.router, prefix="/users", tags=["users"])
-app.include_router(teams.router, prefix="/teams", tags=["teams"])
-app.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
-app.include_router(meetings.router, prefix="/meetings", tags=["meetings"])
-app.include_router(evaluations.router,
-                   prefix="/evaluations", tags=["evaluations"])
-app.include_router(calendar.router, prefix="/cakendar", tags=["calendar"])
+app.include_router(auth.router)
+app.include_router(frontend.router)
+app.include_router(users.router)
+app.include_router(teams.router)
+app.include_router(tasks.router)
+app.include_router(meetings.router)
+app.include_router(evaluations.router)
+app.include_router(calendar.router)
 
-
+from app.admin import setup_admin
 setup_admin(app)
