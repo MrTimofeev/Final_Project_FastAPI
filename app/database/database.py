a47a25a931a8a400d292a9b146260a6da64fbd13
MrTimofeev <nemo.capitan.26ru@mail.ru>
2025-08-28 15:47:12 +0300

    Заменен сихнронные sessionmaker на асинхронный async_sessionmaker

--- app/database/database.py
+++ app/database/database.py
@@ -1,5 +1,5 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
 from decouple import config
 
 
@@ -19,8 +19,10 @@ else:
 engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 
 
-AsyncSessionLocal = sessionmaker(
-    bind=engine, class_=AsyncSession, expire_on_commit=False
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, 
+    class_=AsyncSession, 
+    expire_on_commit=False
 )
 
 
